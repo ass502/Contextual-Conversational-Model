d3a1bd007a8b6e05756439fd68f4c909c9142997
ass502 <ass502@nyu.edu>
2016-11-30 00:49:09 -0500

    removing inner html tags from text

--- data_scraping/convert_xml_to_text_by_movie.py
+++ data_scraping/convert_xml_to_text_by_movie.py
@@ -1,14 +1,14 @@
 from xml.etree import cElementTree as ET
 import os
 import sys
+import re
 
 #track counts 
 sentence_count = 0
 token_count = 0
 
-
 #recursively walk through OpenSubtitles directory
-for root, dirs, files in os.walk('data/en'):
+for root, dirs, files in os.walk('../data/en'):
 	for name in files:
 		#get filename of current file
 		filename = os.path.join(root,name)
@@ -19,7 +19,7 @@ for root, dirs, files in os.walk('data/en'):
 			document = ET.parse(filename)
 			tree = document.getroot()
 
-			with open("./data/processed_en/"+name[:-4]+".txt","wb") as out_f:
+			with open("../data/processed_en/"+name[:-4]+".txt","wb") as out_f:
 
 				#each child of the document tree is a sentence
 				for c,child in enumerate(tree):
@@ -29,6 +29,9 @@ for root, dirs, files in os.walk('data/en'):
 						if child[i].tag == 'w':
 							sentence += child[i].text
 							sentence += " "
+
+							#remove inner html tags like <i>  </i>
+							sentence = re.sub('<[^<]+?>', '', sentence)
 							token_count += 1
 
 					try:
@@ -40,7 +43,7 @@ for root, dirs, files in os.walk('data/en'):
 						pass
 
 					out_f.write(sentence.encode('utf-8')+'\n')
-			
+		
 
 print "Sentence count: " + str(sentence_count)
 print "Token count: " + str(token_count)
